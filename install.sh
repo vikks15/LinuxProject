6d92022fa0c7e2e7d89d7ab5233bb7be3e495678
vikks15 <lva9@mail.ru>
2017-05-17 10:08:43 +0300

    install.sh improve

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 echo "hello"
 
+#root check
 if [ "$EUID" != 0 ]
 	then echo "I need to be root"
 	exec sudo $0 root #replace this programm by
@@ -9,17 +10,27 @@ fi
 
 useradd -m myuser
 passwd -d myuser
-cp cronn /home/myuser/ 
-su - myuser
 
+echo "Starting to install"
+sudo apt install bc
+sudo apt install -y sysstat apache2 libapache2-mod-php
+
+systemctl stop apache2
+
+sudo apt install nginx
+
+echo "Starting to copy files and scripts" 
+cp cronn /home/myuser/
+cp 000-default.conf /etc/apache2/sites-enabled/
+cp default /etc/nginx/sites-enabled/
+cp ports.conf /etc/apache2/ 
+cp index.html /var/www/html/
+cp index.php /var/www/html/
+cp -R sysinfo /var/www/html/
+
+systemctl start apache2
+
+su - myuser
 crontab -l -u myuser | cat /home/myuser/cronn | crontab -u myuser -
 
-#crontab -e * * * * * top -b -n 1|wall
-#crontab u1 -l > cr
-#* * * * * top -b -n 1 | wall >> cr
-#crontab u1 cr 
-#rm cr
-#sudo apt install -y sysstat apache2 libapache2-mod-php
-#apache2 stop
-#sudo apt install nginx
 
